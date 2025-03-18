95551c7d621c3cfe15e9592e35344a892a604c0c
Spyros Zarkadis <spyroszarkadis@gmail.com>
2025-03-18 22:20:13 +0200

    making the basic grid

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const gridContainer = document.querySelector(".grid-container");
 
 function createGrid() {
     for(let i = 0; i < gridSize; i++) {
-      const row = document.createElement("div");
-      row.className = "row";
-      gridContainer.appendChild(row);
-      row.textContent = i;
+      let row = document.createElement("div");
+      row.classList.add("row"); 
 
-    for(let j = 0; j < gridSize; j++) {
-        const column = document.createElement("div");
-        column.className = "column";
-        gridContainer.appendChild(column);
+      for(let j = 0; j < gridSize; j++) {
+        let column = document.createElement("div");
+        column.classList.add("column");
+        row.appendChild(column);
         column.textContent = j;
-    }    
-    }
+      };  
+
+      gridContainer.appendChild(row);
+    };
 }
 createGrid();
