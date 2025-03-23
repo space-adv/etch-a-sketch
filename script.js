691d16ce9de621b86f9e3e998ae993cd8e644da2
Spyros Zarkadis <spyroszarkadis@gmail.com>
2025-03-23 14:16:41 +0200

    changing the size of the grid

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 let gridSize = 16;
-const gridContainer = document.querySelector(".grid-container");
 
-function createGrid() {
+function createGrid(gridSize) {
+  const gridContainer = createGridContainer()
     for(let i = 0; i < gridSize; i++) {
       let row = document.createElement("div");
       row.classList.add("row"); 
-      row.addEventListener("mouseenter", function(e) {
-        console.log(e.target);
-      });
+      
       for(let j = 0; j < gridSize; j++) {
         let column = document.createElement("div");
         column.classList.add("column");
@@ -15,12 +13,35 @@ function createGrid() {
         column.addEventListener("mouseenter", function(e){
           e.target.style.background = "black";
         });
-      };  
-
+      }; 
       gridContainer.appendChild(row);
     };
 
 }
-createGrid();
+createGrid(gridSize);
 
 
+function gridResize() {
+    let input = parseInt (prompt("Choose the grid size between 1 and 100.", "0"));
+    if(input > 100 || input < 0) {
+      gridSize = alert("Invalid size.");
+    } else {
+      const gridContainer = document.querySelector(".grid-container");
+      gridContainer.remove();
+      gridSize = input;
+      createGrid(gridSize);
+      
+    }
+    
+    console.log(gridSize);
+} 
+console.log(gridSize);
+
+function createGridContainer() {
+  const gridContainer = document.createElement("div");
+   gridContainer.classList.add("grid-container");
+   const container = document.querySelector(".container");
+   container.appendChild(gridContainer);
+   return gridContainer;
+   
+}
